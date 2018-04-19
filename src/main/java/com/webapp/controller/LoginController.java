28d9959025644559a0bad8480dc1322974db7162
namangoyal15 <naman_goyal_94@yahoo.com>
2018-04-19 12:11:21 +0530

    Added viewResolver for login.jsp view

--- src/main/java/com/webapp/controller/LoginController.java
+++ src/main/java/com/webapp/controller/LoginController.java
@@ -2,16 +2,15 @@ package com.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController 
 {
+	
 	@RequestMapping(value="/login")
-	@ResponseBody
 	public String sayHello()
 	{
-		return "hello called";
+		return "login";
 	}
 	
 }
